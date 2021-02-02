f958a16bf64701fc8a66a31e6b46742d03dc1ae2
Uriel Romero <urome23@gmail.com>
2021-02-01 20:47:21 -0600

    Added delete All and delete by id controllers

--- src/main/java/guru/springframework/spring5webapp/controllers/TimeTrackerController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/TimeTrackerController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalTime;
 
 @Controller
@@ -17,7 +18,6 @@ public class TimeTrackerController {
     public TimeTrackerController(TimeTrackerRepository timeTrackerRepository) {
         this.timeTrackerRepository = timeTrackerRepository;
     }
-
 
     @RequestMapping("/timetracker")
     public String getMain(Model model){
@@ -35,9 +35,33 @@ public class TimeTrackerController {
         newTime = new TimeTracker(currentTime);
 
         model.addAttribute("timetrackers", timeTrackerRepository.save(newTime));
+
         model.addAttribute("timetrackers", timeTrackerRepository.findAll());
 
-        return "/timetracker/list";
+        return "timetracker/list";
+    }
+
+    @RequestMapping("/deleteEntry")
+    public String deleteEntry(HttpServletRequest request, Model model){
+
+        //handle id entered by user
+        String userInputValue = request.getParameter("xx");
+
+        System.out.println(userInputValue);
+
+        Long x = Long.parseLong(userInputValue);
+
+        timeTrackerRepository.deleteById(x);
+
+        model.addAttribute("timetrackers", timeTrackerRepository.findAll());
+
+        return "timetracker/list";
+    }
+
+    @RequestMapping("/deleteAll")
+    public String deleteAll(){
+        timeTrackerRepository.deleteAll();
+        return "timetracker/list";
     }
 
 
